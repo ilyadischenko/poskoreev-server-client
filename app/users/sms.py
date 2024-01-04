f2eeedf50198407a67ee2fe42b63208550c8f9d2
Krutka-sama <91425892+Krutka-sama@users.noreply.github.com>
2024-01-04 18:23:43 +0300

    Update sms.py

--- app/users/sms.py
+++ app/users/sms.py
@@ -1,11 +1,14 @@
 import datetime
 from users_views import write_code, get_code
+from app.auth.jwt_handler import signJWT, decodeJWT
 
-async def send_sms(number):
-    return await write_code(number, 1234)
+async def send_sms(number : str):
+    code=1234
+    await write_code(number, code)
+    return code
 
 async def confirm_sms(number, code):
     code, time = await get_code(number)
     if code == code and time > datetime.datetime.now():
-        #...
-        return
+        return True
+    else: return False

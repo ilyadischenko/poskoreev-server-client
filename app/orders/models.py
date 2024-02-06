9bf1a80e5cbeaa26e8611f81bc58a96ebf89d255
Krutka-sama <epervyshev@mail.ru>
2024-02-06 16:54:26 +0300

    order update

--- app/orders/models.py
+++ app/orders/models.py
@@ -2,33 +2,24 @@ from tortoise import fields
 from tortoise.models import Model
 
 class Order(Model):
-    user = fields.ForeignKeyField('models.User')
+    user = fields.OneToOneField('models.User')
     #restaurant = fields.ForeignKeyField('models.Restaurant', related_name='restaurant')
-    products = fields.ManyToManyField('models.Menu')
-    type = fields.ForeignKeyField('models.OrderType')
-    order_time = fields.DatetimeField()
-    added_bonuses=fields.IntField(ge=0)
-    promocode = fields.ForeignKeyField('models.PromoCodePercent', null=True)
-    status = fields.ForeignKeyField('models.OrderStatus')
+    items = fields.ManyToManyField('models.CartItem')
+    # 0 - inside 1 - delivery 2 - pickup
+    type = fields.IntField(default=1)
+    invalid_at = fields.DatetimeField()
+    #time = fields.DatetimeField()
+    added_bonuses=fields.IntField(ge=0, default=0)
+    #promocode = fields.ForeignKeyField('models.PromoCodePercent', null=True)
+    #0 - canceled 1 - paid 2 - in progress
+    status = fields.IntField(default=2)
     #address = fields.ForeignKeyField('models.Address')
-    products_count = fields.IntField(ge=0)
-    sum = fields.FloatField(ge=0)
-    total_sum = fields.FloatField(ge=0)
-
-class OrderType(Model):
-    id = fields.IntField(pk=True)
-    type = fields.CharField(max_length=255, unique=True)
-
-class OrderStatus(Model):
-    id = fields.IntField(pk=True)
-    status = fields.CharField(max_length=255, unique=True)
+    products_count = fields.IntField(ge=0, default=0)
+    sum = fields.FloatField(ge=0, default=0)
+    #total_sum = fields.FloatField(ge=0, default=0)
 
 class CartItem(Model):
-    product = fields.ForeignKeyField('models.Menu', unique=True)
+    user = fields.ForeignKeyField('models.User')
+    product = fields.ForeignKeyField('models.Menu')
     quantity = fields.IntField(ge=0,default=0)
     sum = fields.FloatField(ge=0,default=0)
-
-class Cart(Model):
-    user = fields.ForeignKeyField('models.User')
-    items = fields.ManyToManyField('models.CartItem')
-    sum = fields.FloatField(ge=0, default=0)
--- app/orders/views.py
+++ app/orders/views.py
@@ -1,46 +1,55 @@
-from fastapi import HTTPException, APIRouter
-from app.orders.models import Order, OrderType, OrderStatus, Cart, CartItem
+from fastapi import HTTPException, APIRouter, Depends
+from app.orders.models import Order, CartItem
 from app.products.models import Menu
+from app.users.service import AuthGuard, auth
+from datetime import datetime, timezone, timedelta
 orders_router = APIRouter(
     prefix="/api/v1/orders"
 )
 
-@orders_router.get('/getCart', tags=['Orders'])
-async def getCart(user_id : int):
-    cart = await Cart.get_or_none(user_id=user_id)
-    if not cart: return None
+@orders_router.get('/getOrder', tags=['Orders'])
+async def getOrder(user_id: AuthGuard = Depends(auth)):
+    order = await Order.get_or_none(user_id=user_id)
+    if not order: return None
     cart_list=[]
-    for item in await cart.items.all():
+    sum=0
+    count=0
+    for item in await order.items.all():
         cart_list.append({'product_id': item.product_id,
                           'quantity': item.quantity,
                           'sum': item.sum})
-    return cart_list
-@orders_router.post('/addToCart', tags=['Orders'])
-async def addToCart(menu_id: int, quantity: int, user_id : int):
+        count+=item.quantity
+        sum+=item.sum
+        order.products_count=count
+        order.sum=sum
+        await order.save()
+    return {"user":order.pk, "items":cart_list, "sum": sum}
+@orders_router.post('/addToOrder', tags=['Orders'])
+async def addToOrder(menu_id: int, quantity: int, user_id: AuthGuard = Depends(auth)):
     product = await Menu.get_or_none(id=menu_id)
     if not product: raise HTTPException(status_code=404, detail=f"Product {menu_id} not found")
-    item = await CartItem.get_or_none(product_id=product.id)
+    item = await CartItem.get_or_none(product_id=menu_id, user_id=user_id)
     if not item:
-        item=CartItem(product_id=product.id, quantity=quantity, sum=quantity*product.price)
+        item=CartItem(user_id=user_id, product_id=menu_id, quantity=quantity, sum=quantity*product.price)
         await item.save()
     else:
         item.quantity = quantity
         item.sum = product.price*quantity
         await item.save()
-    cart = await Cart.get_or_none(user_id=user_id)
-    if not cart:
-        cart = await Cart.create(user_id=user_id)
-    await cart.items.add(item)
-    return await getCart(user_id)
+    order = await Order.get_or_none(user_id=user_id)
+    if not order:
+        order = await Order.create(user_id=user_id, invalid_at=datetime.now()+timedelta(days=1))
+    await order.items.add(item)
+    return await getOrder(user_id)
 
 @orders_router.delete('/removeFromCart', tags=['Orders'])
-async def removeFromCart(menu_id: int, user_id : int):
-    cart = await Cart.get_or_none(user_id=user_id)
-    if not cart: return None
+async def removeFromCart(menu_id: int, user_id: AuthGuard = Depends(auth)):
+    order = await Order.get_or_none(user_id=user_id)
+    if not order: return None
     product = await Menu.get_or_none(id=menu_id)
     if not product: raise HTTPException(status_code=404, detail=f"Product {menu_id} not found")
-    item = await CartItem.get_or_none(product_id=product.id)
+    item = await CartItem.get_or_none(product_id=menu_id, user_id=user_id)
     if not item: raise HTTPException(status_code=404, detail="No such product")
-    await cart.items.remove(item)
+    await order.items.remove(item)
     await item.delete()
-    return await getCart(user_id)
+    return await getOrder(user_id)

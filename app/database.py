2b44f3ebfcb71622ca86c3d134e1b7f6db98fbff
IlyaDischenko <ilyadischenko@gmail.com>
2024-06-25 19:13:31 +0300

    fix

--- app/database.py
+++ app/database.py
@@ -19,6 +19,7 @@ TORTOISE_ORM = {
         # "default": f"postgres://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}",
         "default": "postgres://gen_user:12345678a@82.97.255.65:5432/pizza",
     },
+
     "apps": {
         "models": {
             "models": ["app.users.models", "app.promocodes.models", "app.products.models", "app.restaurants.models",
